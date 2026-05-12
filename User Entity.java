2c8aff01c980a8c3ed87cd00ad7396caebcf6bfc
Madhankumarajay <152497154+Madhankumarajay@users.noreply.github.com>
2026-05-12 23:00:48 +0530

    Create User Entity.java

---
+++ User Entity.java
@@ -0,0 +1,23 @@
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
